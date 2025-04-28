7faffbe687d88077cecbfe8445145349d752d577
Denyl911 <imdenyl@gmail.com>
2025-04-27 21:28:24 -0600

    fix db port

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	dialect: 'mysql',
 	dbCredentials: {
 		host: process.env.SCANTEATE_DB_HOST || '',
-		port: 5432,
+		port: 3306,
 		database: process.env.SCANTEATE_DB_NAME || '',
 		user: process.env.SCANTEATE_DB_USER || '',
 		password: process.env.SCANTEATE_DB_PASS || '',
